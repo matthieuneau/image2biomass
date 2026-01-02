8cd23d7690cc7c04dedb0a47b34c0f3bac7920e4
= <=>
2026-01-02 23:36:12 +0100

    add get model utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import torch
 
+from models import *
+
 _standard_repr = torch.Tensor.__repr__
 
 
@@ -10,3 +12,7 @@ def enhanced_repr(self) -> str:
     grad = "🔥" if self.requires_grad else "❄️"
     mem = f"{self.element_size() * self.nelement() / 1024:.1f}KB"
     return f"[{shape} | {device} | {dtype} | {grad} | {mem}] -> {_standard_repr(self)}"
+
+
+def get_model(config: dict) -> torch.nn.Module:
+    return eval(config["model_class"])(model_name=config["model_name"])
